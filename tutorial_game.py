3202b1601dab516b701d33c9749d23c56bdd0606
Alex S <Alexander2.Silvester@live.uwe.ac.uk>
2023-10-12 12:03:44 +0100

    did worksheet

--- tutorial_game.py
+++ tutorial_game.py
@@ -4,7 +4,12 @@ from gamedata import GameData
 
 
 def isInside(sprite, mouse_x, mouse_y) -> bool:
-    pass
+    bounds = sprite.getWorldBounds()
+
+    if bounds.v1.x < mouse_x < bounds.v2.x and bounds.v1.y < mouse_y < bounds.v3.y:
+        return True
+
+    return False
 
 
 class MyASGEGame(pyasge.ASGEGame):
@@ -54,26 +59,88 @@ class MyASGEGame(pyasge.ASGEGame):
         self.fish = pyasge.Sprite()
         self.initFish()
 
+        self.direction = 1
+
     def initBackground(self) -> bool:
-        pass
+        if self.data.background.loadTexture("/data/images/background.png"):
+            self.data.background.z_order = -100
+            return True
+        else:
+            return False
 
     def initFish(self) -> bool:
-        pass
+        if self.fish.loadTexture("/data/images/kenney_fishpack/fishTile_073.png"):
+            self.fish.z_order = 1
+            self.fish.scale = 1
+            self.spawn()
+            return True
+        return False
 
     def initScoreboard(self) -> None:
-        pass
+        self.scoreboard = pyasge.Text(self.data.fonts["mainFont"])
+        self.scoreboard.x = 1300
+        self.scoreboard.y = 75
+        self.scoreboard.string = str(self.data.score).zfill(6)
 
     def initMenu(self) -> bool:
-        pass
+        self.data.fonts["mainFont"] = self.data.renderer.loadFont("/data/fonts/KGHAPPY.ttf", 64)
+
+        self.menu_text = pyasge.Text(self.data.fonts["mainFont"])
+        self.menu_text.string = "The Fish Game"
+        self.menu_text.position = [100, 100]
+        self.menu_text.colour = pyasge.COLOURS.HOTPINK
+
+        self.exit_option = pyasge.Text(self.data.fonts["mainFont"])
+        self.exit_option.string = "Exit"
+        self.exit_option.position = [500, 400]
+        self.exit_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
+
+        self.play_option = pyasge.Text(self.data.fonts["mainFont"])
+        self.play_option.string = "Start"
+        self.play_option.position = [100, 400]
+        self.play_option.colour = pyasge.COLOURS.HOTPINK
+
+        return True
 
     def clickHandler(self, event: pyasge.ClickEvent) -> None:
-        pass
+        if event.action == pyasge.MOUSE.BUTTON_PRESSED and \
+                event.button == pyasge.MOUSE.MOUSE_BTN1:
+
+            if isInside(self.fish, event.x, event.y):
+                self.data.score += 1
+                self.scoreboard.string = str(self.data.score).zfill(6)
+                self.spawn()
 
     def keyHandler(self, event: pyasge.KeyEvent) -> None:
-        pass
+
+        if event.action == pyasge.KEYS.KEY_PRESSED:
+
+            if event.key == pyasge.KEYS.KEY_RIGHT or event.key == pyasge.KEYS.KEY_LEFT:
+
+                self.menu_option = 1 - self.menu_option
+                if self.menu_option == 0:
+                    self.play_option.string = "Start"
+                    self.play_option.colour = pyasge.COLOURS.HOTPINK
+                    self.exit_option.string = "Exit"
+                    self.exit_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
+                else:
+                    self.play_option.string = "Start"
+                    self.play_option.colour = pyasge.COLOURS.LIGHTSLATEGRAY
+                    self.exit_option.string = "Exit"
+                    self.exit_option.colour = pyasge.COLOURS.HOTPINK
+
+            if event.key == pyasge.KEYS.KEY_ENTER:
+                if self.menu_option == 0:
+                    self.menu = False
+                else:
+                    self.signalExit()
 
     def spawn(self) -> None:
-        pass
+        x = random.randint(0, self.data.game_res[0] - self.fish.width)
+        y = random.randint(0, self.data.game_res[1] - self.fish.height)
+
+        self.fish.x = x
+        self.fish.y = y
 
     def update(self, game_time: pyasge.GameTime) -> None:
 
@@ -81,7 +148,10 @@ class MyASGEGame(pyasge.ASGEGame):
             # update the menu here
             pass
         else:
-            # update the game here
+
+            if self.fish.x == 0 or self.fish.x == 1600 - self.fish.width:
+                self.direction *= -1
+            self.fish.x += self.direction
             pass
 
     def render(self, game_time: pyasge.GameTime) -> None:
@@ -94,9 +164,17 @@ class MyASGEGame(pyasge.ASGEGame):
 
         if self.menu:
             # render the menu here
+            self.data.renderer.render(self.data.background)
+            self.data.renderer.render(self.menu_text)
+            self.data.renderer.render(self.exit_option)
+            self.data.renderer.render(self.play_option)
             pass
+
         else:
             # render the game here
+            self.data.renderer.render(self.data.background)
+            self.data.renderer.render(self.scoreboard)
+            self.data.renderer.render(self.fish)
             pass
 
 
@@ -109,6 +187,7 @@ def main():
     the FPS and fixed updates are capped at 60hz and Vsync is
     set to adaptive.
     """
+
     settings = pyasge.GameSettings()
     settings.window_width = 1600
     settings.window_height = 900
